c69cfc93c647b9ab703a5a696866ed4f759e7b39
Wanderlei Neves Junior <51794805+wnjinfo@users.noreply.github.com>
2026-03-02 15:36:07 -0300

    Update config.js

--- equipechat/backend/config/config.js
+++ equipechat/backend/config/config.js
@@ -1,1 +1,28 @@
+require('dotenv').config(); // si usas .env, opcional
 
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || "mi_base",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "postgres",
+    port: process.env.DB_PORT || 5432
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "postgres",
+    port: process.env.DB_PORT || 5432
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  }
+};
